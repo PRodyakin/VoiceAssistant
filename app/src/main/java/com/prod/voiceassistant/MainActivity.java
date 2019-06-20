7bc59b0c8b0bca831ff37a9d685922cbccf407be
DataLoss <data.loss@yandex.ru>
2019-06-20 18:00:48 +0300

    voice and ui

--- app/src/main/java/com/prod/voiceassistant/MainActivity.java
+++ app/src/main/java/com/prod/voiceassistant/MainActivity.java
@@ -1,5 +1,6 @@
 package com.prod.voiceassistant;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v4.util.Consumer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,11 +9,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     protected Button sendButton;
     protected EditText questionField;
     protected TextView chatWindow;
+
+    protected TextToSpeech tts;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -32,19 +37,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR){
+                    tts.setLanguage(new Locale("ru"));
+                }
+            }
+        });
+
     }
 
     protected void onClickSendButton(){
 
         String text = questionField.getText().toString();
+        chatWindow.append(">> "+ text + "\n");
         AI.getAnswer(text, new Consumer<String>() {
             @Override
             public void accept(String answer) {
                 chatWindow.append("<< "+ answer + "\n");
+                tts.speak(answer, TextToSpeech.QUEUE_FLUSH, null,null);
             }
         });
 
-        chatWindow.append(">> "+ text + "\n");
+
        // chatWindow.append();
         questionField.setText("");
 
